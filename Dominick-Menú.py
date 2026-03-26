dbaf04e71ff027b721b16ee926798cef461bfa79
Dominick-RS <dominickrsamudio@gmail.com>
2026-03-25 23:54:02 -0600

    Creación de la ventana de análisis y su botón
    Creación del botón de análisis de números en el menú, configuración de la ventana de análisis y botón para regresar al menú

--- Dominick-Menú.py
+++ Dominick-Menú.py
@@ -11,10 +11,46 @@ contador = 0
 def esta_abierta(v):
     return v is not None and v.winfo_exists()
 
+#Funcion para la ventana de analisis
+def crear_ventana_Num():
+    global Ventana_Num, contador
+    Ventana_Num = tk.Toplevel(menu)                  #Configuracion de la ventana de analisis
+    Ventana_Num.title("Análisis de números")
+    Ventana_Num.geometry("600x500")
+    Ventana_Num.resizable(False, False)
+    Ventana_Num.protocol("WM_DELETE_WINDOW", cerrar_ventana_Num)   #Manejo de ventanas
+    contador += 1    #Contador de ventana abierta
+
+ #Canva de la ventana de análisis
+    canva_num = tk.Canvas(Ventana_Num, width=600, height=500, bg="white")
+    canva_num.pack()
+
+ #Texto de análisis
+    Analisis = tk.Label(canva_num, text="Análisis de números ", background="white", font=("verdana", 18, "bold"))
+    Analisis.place(x=150, y=10)
+
+ #Boton para volver al menú
+    Volver_M = tk.Button(canva_num, text="Volver al Menú", command=lambda:cerrar_ventana_Num())
+    Volver_M.place(x=250, y=400 )
+
+#Funcion para abir la ventana de análisi
+def abrir_ventana_Num():
+    esta_abierta(Ventana_Num) and Ventana_Num.lift()
+    (not esta_abierta(Ventana_Num)) and (contador >= 2) and messagebox.showwarning("Límite", "Máximo 2 ventanas abiertas.")
+    (not esta_abierta(Ventana_Num)) and (contador < 2) and crear_ventana_Num()
+
+#Funcion para volver al menú y cerrar la ventana de análisis
+def cerrar_ventana_Num():
+    global Ventana_Num, contador
+    Ventana_Num.destroy()
+    Ventana_Num = None
+    contador -= 1   #Disminución en el contador
+    menu.lift()
+
 #Configuracion del menu
 menu = tk.Tk()
 menu.title ("Menú")
-menu.geometry("800x600")
+menu.geometry("600x600")
 menu.resizable(width=False, height=False)
 
 #Texto del menu
@@ -24,6 +60,10 @@ label.pack(pady=10)
 #Canva del menu
 canva_m = tk.Canvas(menu, width=800, height=600, bg="white")
 canva_m.pack()
+
+#Boton para abrir el analisis de números
+AnalizarN = tk.Button(canva_m, text="Abrir análisis de números", width=20, command=lambda: abrir_ventana_Num())
+AnalizarN.place(x=20, y= 20)
 
 
 menu.mainloop()
