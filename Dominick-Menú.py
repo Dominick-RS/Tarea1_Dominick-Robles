2bbbcb9906a4f08352ed706d6c6b20403904cfd4
Dominick-RS <dominickrsamudio@gmail.com>
2026-03-26 01:40:35 -0600

    Añadí la ventana  de información
    Añadí la ventana de información y su configuración

--- Dominick-Menú.py
+++ Dominick-Menú.py
@@ -11,19 +11,94 @@ contador = 0
 def esta_abierta(v):
     return v is not None and v.winfo_exists()
 
-#Configuracion del menu
+#Funcion para la ventana de analisis
+def crear_ventana_Num():
+    global Ventana_Num, contador
+    Ventana_Num = tk.Toplevel(menu)                  #Configuracion de la ventana de analisis
+    Ventana_Num.title("Análisis de números")
+    Ventana_Num.geometry("600x500")
+    Ventana_Num.resizable(False, False)
+    Ventana_Num.protocol("WM_DELETE_WINDOW", cerrar_ventana_Num)   #Manejo de ventanas
+    contador += 1    #Contador de ventana abierta
+
+ #Canva de la ventana de análisis
+    canva_num = tk.Canvas(Ventana_Num, width=600, height=500, bg="white")
+    canva_num.pack()
+
+ #Texto de análisis
+    Analisis = tk.Label(canva_num, text="Análisis de números ", background="white", font=("verdana", 18, "bold"))
+    Analisis.place(x=150, y=10)
+
+ #Boton para volver al menú
+    Volver_M = tk.Button(canva_num, text="Volver al Menú", command=lambda:cerrar_ventana_Num())
+    Volver_M.place(x=250, y=400)
+
+#Funcion para abir la ventana de análisi
+def abrir_ventana_Num():
+    esta_abierta(Ventana_Num) and Ventana_Num.lift()
+    (not esta_abierta(Ventana_Num)) and (contador >= 1) and messagebox.showwarning("Límite", "Máximo 2 ventanas abiertas.")
+    (not esta_abierta(Ventana_Num)) and (contador < 1) and crear_ventana_Num()
+
+#Funcion para volver al menú y cerrar la ventana de análisis
+def cerrar_ventana_Num():
+    global Ventana_Num, contador
+    Ventana_Num.destroy()
+    Ventana_Num = None
+    contador -= 1   #Disminución en el contador
+    menu.lift()
+
+#Funcion para crear la ventana de información
+def crear_ventana_Info():
+    global ventana_Info, contador
+    ventana_Info = tk.Toplevel(menu)          #Configuración de la ventana información
+    ventana_Info.title("Información")
+    ventana_Info.geometry("600x500")
+    ventana_Info.resizable(False, False)
+    ventana_Info.protocol("WM_DELETE_WINDOW", cerrar_ventana_Info)
+    contador += 1
+
+    canva_i = tk.Canvas(ventana_Info, width= 600, height= 500, bg="white")
+    canva_i.pack()
+
+    labeli = tk.Label(canva_i,text="información", background="white", font=("verdana", 18, "bold"))
+    labeli.place(x=200, y= 10)
+
+    Volver_i = tk.Button(canva_i, text="Volver al Menú", command=lambda:cerrar_ventana_Info())
+    Volver_i.place(x=250, y=400 )
+
+def abrir_ventana_Info():
+    esta_abierta(ventana_Info) and ventana_Info.lift()
+    (not esta_abierta(ventana_Info)) and (contador >= 1) and messagebox.showwarning("Límite", "Máximo 2 ventanas abiertas.")
+    (not esta_abierta(ventana_Info)) and (contador < 1) and crear_ventana_Info()
+
+def cerrar_ventana_Info():
+    global ventana_Info, contador
+    ventana_Info.destroy()
+    ventana_Info = None
+    contador -= 1
+    menu.lift()
+
+
+#Configuracion del menú
 menu = tk.Tk()
 menu.title ("Menú")
-menu.geometry("800x600")
+menu.geometry("600x600")
 menu.resizable(width=False, height=False)
 
 #Texto del menu
-label = tk.Label(menu, text="Menú", font=("Verdana", 20, "bold"))
-label.pack(pady=10)
+labelm = tk.Label(menu, text="Menú", font=("Verdana", 20, "bold"))
+labelm.pack(pady=10)
 
 #Canva del menu
 canva_m = tk.Canvas(menu, width=800, height=600, bg="white")
 canva_m.pack()
 
+#Boton para abrir el análisis de números
+AnalizarN = tk.Button(canva_m, text="Abrir análisis de números", width=20, command=lambda: abrir_ventana_Num())
+AnalizarN.place(x=20, y=20)
+
+#Boton para ver la información personal
+Infor = tk.Button(canva_m, text="Mostrar información", width=20, command=lambda: abrir_ventana_Info())
+Infor.place(x=220, y=20)
 
 menu.mainloop()
